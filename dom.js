df51c97b8652152b710d2f17638a6f2b6a04932a
123kishan <mishrakishan085@gmail.com>
2023-06-14 07:03:04 +0530

    changing dom.js

--- dom.js
+++ dom.js
@@ -1,6 +1,15 @@
-var item=document.getElementsByClassName('list-group-item');
+/*var item=document.getElementsByClassName('list-group-item');
 item[2].style.backgroundColor="green";
 for(let i=0;i<5;i++)
 {
     item[i].style.fontWeight="bold";
+}*/
+var item=document.querySelectorAll('.list-group-item');
+/*item[1].style.backgroundColor="green";
+item[2].style.display="none";*/
+item[1].style.color="green";
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green";
 }
